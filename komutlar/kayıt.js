5e3749347b26f45128b7880df7b9d99a9574f28a
/shynox <66389292+BilalTaner@users.noreply.github.com>
2020-09-06 23:10:04 +0300

    Update kayıt.js

--- komutlar/kayıt.js
+++ komutlar/kayıt.js
@@ -28,14 +28,15 @@ const ayarlar = require('../ayarlar.json');
     const misafir = message.guild.roles.get(db.fetch(`rol31_${message.guild.id}`)); //buraya kayıtsız rolü gelir
     const sorumlu = message.guild.roles.get(db.fetch(`rol3_${message.guild.id}`)); //buraya kayıt sorumlusu rolü gelir
     const log = message.guild.channels.get(db.fetch(`kayıtlog_${message.guild.id}`)); //buraya kayıt edilen kişinin log bilgileri vb düşer.
-    const kayıtsay = db.fetch(`toplamkayıt.${message.author.id}.${message.guild.id}`); //yapılan kayıt sayısının fetchlenmesi
-    if (!sorumlu) return message.channel.send(new Discord.RichEmbed().setDescription(`**Sorumlu Rolü Ayarlanmamış!\nAyaralamak için \`${prefix}kayıtçı-rol @rol\`**`).setColor('BLACK')).then(msg => msg.delete(10000));
-    if (!kayıtlı1) return message.channel.send(new Discord.RichEmbed().setDescription(`**Kayıtlı-Rol(1) Ayarlanmamış!\nAyaralamak için \`${prefix}kayıtlı-rol 1 @rol\`**`).setColor('BLACK')).then(msg => msg.delete(10000));
+    if (!sorumlu) return message.channel.send(new Discord.RichEmbed().setDescription(`**Sorumlu Rolü Ayarlanmamış!\n Ayaralamak için \`${prefix}kayıtçı-rol @rol\`**`).setColor('BLACK')).then(msg => msg.delete(10000));
+    if (!kayıtlı1) return message.channel.send(new Discord.RichEmbed().setDescription(`**Kayıtlı-Rol(1) Ayarlanmamış!\n Ayaralamak için \`${prefix}kayıtlı-rol 1 @rol\`**`).setColor('BLACK')).then(msg => msg.delete(10000));
     if(!message.member.roles.get(db.fetch(`rol3_${message.guild.id}`))) { //buraya kayıt sorumlusu rolünün id'sini giriniz.
       return message.channel.send(new Discord.RichEmbed().setDescription("Bu işlemi sadece Ayarlanmış Kayıt Sorumluları gerçekleştirebilir.").setColor('BLACK')).then(msg => msg.delete(10000));
     } else {
       let member = message.mentions.users.first() || client.users.get(args.join(' '))
-        if(!member) return message.channel.send(new Discord.RichEmbed().setDescription("Bir kullanıcı girin.").setColor('BLACK'))
+      db.add(`toplamkayıt.${message.author.id}.${message.guild.id}`, 1)//kayıt sayınıza 1 eklenir
+        if(!member) return message.channel.send(new Discord.RichEmbed().setDescription("Bir kullanıcı girin.").setColor('BLACK')).then(msg => msg.delete(5000));
+      const kayıtsay = db.fetch(`toplamkayıt.${message.author.id}.${message.guild.id}`); //yapılan kayıt sayısının fetchlenmesi
       const c = message.guild.member(member)
       const nick = args[2];
       const yas = args[3];
@@ -49,23 +50,25 @@ const ayarlar = require('../ayarlar.json');
     .setFooter(message.author.tag , message.author.avatarURL)
     .setTimestamp()
       .setColor('BLACK')
-      log.send(embed)
-      db.add(`toplamkayıt.${message.author.id}.${message.guild.id}`, 1)
+      log.send(embed)   
     }
   }
+
+
    if(args[0] === "2") {//kayıt 2
     const kayıtlı2 = message.guild.roles.get(db.fetch(`kayıtlı2_${message.guild.id}`)); //buraya 2. kayıtlı rolünü gelir
     const misafir = message.guild.roles.get(db.fetch(`rol31_${message.guild.id}`)); //buraya kayıtsız rolü gelir
     const sorumlu = message.guild.roles.get(db.fetch(`rol3_${message.guild.id}`)); //buraya kayıt sorumlusu rolü gelir
     const log = message.guild.channels.get(db.fetch(`kayıtlog_${message.guild.id}`)); //buraya kayıt edilen kişinin log bilgileri vb düşer.
-    const kayıtsay = db.fetch(`toplamkayıt.${message.author.id}.${message.guild.id}`); //yapılan kayıt sayısının fetchlenmesi
-    if (!sorumlu) return message.channel.send(new Discord.RichEmbed().setDescription(`**Sorumlu Rolü Ayarlanmamış!\nAyaralamak için \`${prefix}kayıtçı-rol @rol\`**`).setColor('BLACK')).then(msg => msg.delete(10000));
-    if (!kayıtlı2) return message.channel.send(new Discord.RichEmbed().setDescription(`**Kayıtlı-Rol(2) Ayarlanmamış!\nAyaralamak için \`${prefix}kayıtlı-rol 2 @rol\`**`).setColor('BLACK')).then(msg => msg.delete(8000));
+    if (!sorumlu) return message.channel.send(new Discord.RichEmbed().setDescription(`**Sorumlu Rolü Ayarlanmamış!\n Ayaralamak için \`${prefix}kayıtçı-rol @rol\`**`).setColor('BLACK')).then(msg => msg.delete(10000));
+    if (!kayıtlı2) return message.channel.send(new Discord.RichEmbed().setDescription(`**Kayıtlı-Rol(2) Ayarlanmamış!\n Ayaralamak için \`${prefix}kayıtlı-rol 2 @rol\`**`).setColor('BLACK')).then(msg => msg.delete(8000));
     if(!message.member.roles.get(db.fetch(`rol3_${message.guild.id}`))) { //buraya kayıt sorumlusu rolünün id'sini giriniz.
       return message.channel.send(new Discord.RichEmbed().setDescription("Bu işlemi sadece Ayarlanmış Kayıt Sorumluları gerçekleştirebilir.").setColor('BLACK')).then(msg => msg.delete(10000));
     } else {
       let member = message.mentions.users.first() || client.users.get(args.join(' '))
-        if(!member) return message.channel.send(new Discord.RichEmbed().setDescription("Bir kullanıcı girin.").setColor('BLACK'))
+      db.add(`toplamkayıt.${message.author.id}.${message.guild.id}`, 1)//kayıt sayınıza 1 eklenir
+        if(!member) return message.channel.send(new Discord.RichEmbed().setDescription("Bir kullanıcı girin.").setColor('BLACK')).then(msg => msg.delete(5000));
+        const kayıtsay = db.fetch(`toplamkayıt.${message.author.id}.${message.guild.id}`); //yapılan kayıt sayısının fetchlenmesi
       const c = message.guild.member(member)
       const nick = args[2];
       const yas = args[3];
@@ -80,22 +83,24 @@ const ayarlar = require('../ayarlar.json');
     .setTimestamp()
       .setColor('BLACK')
       log.send(embed)
-	  db.add(`toplamkayıt.${message.author.id}.${message.guild.id}`, 1)
     }
   }
+
+
     if(args[0] === "3") {//kayıt 2
       const kayıtlı3 = message.guild.roles.get(db.fetch(`kayıtlı3_${message.guild.id}`));  //buraya 3. kayıtlı rolünü gelir
       const misafir = message.guild.roles.get(db.fetch(`rol31_${message.guild.id}`)); //buraya kayıtsız rolü gelir
       const sorumlu = message.guild.roles.get(db.fetch(`rol3_${message.guild.id}`)); //buraya kayıt sorumlusu rolü gelir
       const log = message.guild.channels.get(db.fetch(`kayıtlog_${message.guild.id}`)); //buraya kayıt edilen kişinin log bilgileri vb düşer.
-      const kayıtsay = db.fetch(`toplamkayıt.${message.author.id}.${message.guild.id}`); //yapılan kayıt sayısının fetchlenmesi
-      if (!sorumlu) return message.channel.send(new Discord.RichEmbed().setDescription(`**Sorumlu Rolü Ayarlanmamış!\nAyaralamak için \`${prefix}kayıtçı-rol @rol\`**`).setColor('BLACK')).then(msg => msg.delete(10000));
-      if (!kayıtlı3) return message.channel.send(new Discord.RichEmbed().setDescription(`**Kayıtlı-Rol(3) Ayarlanmamış!\nAyaralamak için \`${prefix}kayıtlı-rol 3 @rol\`**`).setColor('BLACK')).then(msg => msg.delete(8000));
+      if (!sorumlu) return message.channel.send(new Discord.RichEmbed().setDescription(`**Sorumlu Rolü Ayarlanmamış!\n Ayaralamak için \`${prefix}kayıtçı-rol @rol\`**`).setColor('BLACK')).then(msg => msg.delete(10000));
+      if (!kayıtlı3) return message.channel.send(new Discord.RichEmbed().setDescription(`**Kayıtlı-Rol(3) Ayarlanmamış!\n Ayaralamak için \`${prefix}kayıtlı-rol 3 @rol\`**`).setColor('BLACK')).then(msg => msg.delete(8000));
       if(!message.member.roles.get(db.fetch(`rol3_${message.guild.id}`))) { //buraya kayıt sorumlusu rolünün id'sini giriniz.
         return message.channel.send(new Discord.RichEmbed().setDescription("Bu işlemi sadece Ayarlanmış Kayıt Sorumluları gerçekleştirebilir.").setColor('BLACK')).then(msg => msg.delete(10000));
       } else {
         let member = message.mentions.users.first() || client.users.get(args.join(' '))
-          if(!member) return message.channel.send(new Discord.RichEmbed().setDescription("Bir kullanıcı girin.").setColor('BLACK'))
+        db.add(`toplamkayıt.${message.author.id}.${message.guild.id}`, 1)//kayıt sayınıza 1 eklenir
+          if(!member) return message.channel.send(new Discord.RichEmbed().setDescription("Bir kullanıcı girin.").setColor('BLACK')).then(msg => msg.delete(5000));
+        const kayıtsay = db.fetch(`toplamkayıt.${message.author.id}.${message.guild.id}`); //yapılan kayıt sayısının fetchlenmesi
         const c = message.guild.member(member)
         const nick = args[2];
         const yas = args[3];
@@ -110,7 +115,8 @@ const ayarlar = require('../ayarlar.json');
       .setTimestamp()
         .setColor('BLACK')
         log.send(embed)
-      db.add(`toplamkayıt.${message.author.id}.${message.guild.id}`, 1)
+      
+      
       }
     }
 
